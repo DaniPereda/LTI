03140010b95193057cdf23be1fdaa77c035413fb
Pereda, Daniel <daniel.pereda@sky.uk>
2025-03-09 14:36:07 +0000

    first commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -30,6 +30,7 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.springframework.security:spring-security-test")
+    testImplementation("com.h2database:h2:2.2.220")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
--- src/test/kotlin/com/dani/lti/ltibackend/LtiBackendApplicationTests.kt
+++ src/test/kotlin/com/dani/lti/ltibackend/LtiBackendApplicationTests.kt
@@ -2,8 +2,10 @@ package com.dani.lti.ltibackend
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
 @SpringBootTest
+@ActiveProfiles("test")
 class LtiBackendApplicationTests {
 
     @Test
